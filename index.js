ab334ed3c5784d6a6b2a1178815946e1a96fbede
Fival <thefivalco@gmail.com>
2021-02-17 15:55:57 -0600

    update

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const inquirer = require('inquirer')
 const Employee = require("./lib/Employee.js")
+const Manager = require("./lib/Manager.js")
+const Engineer = require("./lib/Engineer.js")
+const Intern = require("./lib/Intern.js")
 
-const promptQuestions = () => {
+const promptManager = () => {
     return inquirer.prompt([
         {
          type: "input",
          name: "name",
-         message: "Enter employee name."
+         message: "Enter Manager name."
         },
 
         {
@@ -19,16 +22,42 @@ const promptQuestions = () => {
          type: "input",
          name: "email",
          message: "Enter employee Email."
+        },
+
+        {
+         type: "input",
+         name: "office",
+         message: "Enter office number."
         }
     ])
 }
-promptQuestions()
+
+const promptMenu = () => {
+    return inquirer.prompt([
+        {
+         type: "checkbox",
+         name: "menu",
+         choices: ["Add Engineer.", "Add Intern.", "Finish building team."]
+        }
+    ])
+
+    
+    
+}
+
+promptManager()
     .then( answer => {
-        const employee = new Employee(answer)
+        const employee = new Manager(answer)
         console.table(employee)
     })
+    .then(promptMenu)
+    //.then(promptAnswer => console.log(promptAnswer)
+    
 
-module.exports = promptQuestions
+    
+
+module.exports = promptManager
+
 
 
 
