2e3141912cb622860e5b5842ffaf986f51cd0ae2
Fival <thefivalco@gmail.com>
2021-02-18 15:36:56 -0600

    update

--- index.js
+++ index.js
@@ -4,163 +4,186 @@ const Manager = require("./lib/Manager.js")
 const Engineer = require("./lib/Engineer.js")
 const Intern = require("./lib/Intern.js")
 const generatePage = require("./src/template.js")
+const fs = require('fs')
 
-const askData = []
-
-
-
-
+let managerInfo = null
+let internInfo = null
+let engineerInfo = null
 
 const promptManager = () => {
-    return inquirer.prompt([
-        {
-         type: "input",
-         name: "name",
-         message: "Enter Manager name."
+    return inquirer.prompt([{
+            type: "input",
+            name: "name",
+            message: "Enter Manager name."
         },
 
         {
-         type: "input",
-         name: "id",
-         message: "Enter employee ID."
+            type: "input",
+            name: "id",
+            message: "Enter employee ID."
         },
 
         {
-         type: "input",
-         name: "email",
-         message: "Enter employee Email."
+            type: "input",
+            name: "email",
+            message: "Enter employee Email."
         },
 
         {
-         type: "input",
-         name: "office",
-         message: "Enter office number."
+            type: "input",
+            name: "office",
+            message: "Enter office number."
         }
     ])
 }
 
 const promptMenu = () => {
-    return inquirer.prompt([
-        {
-         type: "list",
-         name: "menu",
-         choices: ["Add Engineer", "Add Intern", "Finish building team"]
-        }
-    ])
+    return inquirer.prompt([{
+        type: "list",
+        name: "menu",
+        choices: ["Add Engineer", "Add Intern", "Finish building team"]
+    }])
 }
 
 const promptEngineer = () => {
-    return inquirer.prompt([
-        {
-         type: "input",
-         name: "name",
-         message: "Enter Engineer name."
+    return inquirer.prompt([{
+            type: "input",
+            name: "name",
+            message: "Enter Engineer name."
         },
 
         {
-         type: "input",
-         name: "id",
-         message: "Enter Engineer ID."
+            type: "input",
+            name: "id",
+            message: "Enter Engineer ID."
         },
 
         {
-         type: "input",
-         name: "email",
-         message: "Enter Engineer Email."
+            type: "input",
+            name: "email",
+            message: "Enter Engineer Email."
         },
 
         {
-         type: "input",
-         name: "github",
-         message: "Enter Github link."
+            type: "input",
+            name: "github",
+            message: "Enter Github link."
         }
     ])
 }
 
 const promptIntern = () => {
-    return inquirer.prompt([
-        {
-         type: "input",
-         name: "name",
-         message: "Enter intern name."
+    return inquirer.prompt([{
+            type: "input",
+            name: "name",
+            message: "Enter intern name."
         },
 
         {
-         type: "input",
-         name: "id",
-         message: "Enter intern ID."
+            type: "input",
+            name: "id",
+            message: "Enter intern ID."
         },
 
         {
-         type: "input",
-         name: "email",
-         message: "Enter intern Email."
+            type: "input",
+            name: "email",
+            message: "Enter intern Email."
         },
 
         {
-         type: "input",
-         name: "school",
-         message: "Enter school attended."
+            type: "input",
+            name: "school",
+            message: "Enter school attended."
         }
     ])
 }
 
 promptManager()
-    
-    .then( answer => {
+
+    .then(answer => {
         const employee = new Manager(answer)
+        managerInfo = employee
         console.table(employee)
         console.log(employee.name)
     })
     .then(promptMenu)
     .then(answer => {
-        if (answer.menu === "Add Engineer" ) {
+        if (answer.menu === "Add Engineer") {
             promptEngineer()
                 .then(answer => {
                     const engineer = new Engineer(answer)
+                    engineerInfo = engineer
                     console.table(engineer)
                 })
                 .then(promptMenu)
                 .then(answer => {
-                    if (answer.menu === "Add Intern" ) {
+                    if (answer.menu === "Add Intern") {
                         promptIntern()
                             .then(answer => {
                                 const intern = new Intern(answer)
+                                internInfo = intern
                                 console.table(intern)
                             })
-                    }
-                })
-        }
-        else { 
-            if (answer.menu === "Add Intern" ) {
-            promptIntern()
-                .then(answer => {
-                    const intern = new Intern(answer)
-                    console.table(intern)
-                })
-                .then(promptMenu)
-                .then(answer => {
-                    if (answer.menu === "Add Engineer" ) {
-                        promptEngineer()
+                            .then(promptMenu)
                             .then(answer => {
-                                const engineer = new Engineer(answer)
-                                console.table(engineer)
+                                if (answer.menu === "Finish building team") {
+                                    generatePage(managerInfo, internInfo, engineerInfo)
+                                    console.log(generatePage(managerInfo, internInfo, engineerInfo))
+                                    fs.writeFile('index.html', generatePage(managerInfo, internInfo, engineerInfo), err => {
+                                        if (err) throw err;
+                                    })
+                                }
                             })
                     }
                 })
-            }
-            else { 
-            if (answer.menu === "Finish building team" ) {
-            generatePage(employee, intern, engineer)
-            console.log(generatePage(employee, intern, engineer))
-            }   
-            }     
-        
-        }
-        
-    })
-    
-    
-    
+        } else {
+            if (answer.menu === "Add Intern") {
+                promptIntern()
+                    .then(answer => {
+                        const intern = new Intern(answer)
+                        internInfo = intern
+                        console.table(intern)
+                    })
+                    .then(promptMenu)
+                    .then(answer => {
+                        if (answer.menu === "Add Engineer") {
+                            promptEngineer()
+                                .then(answer => {
+                                    const engineer = new Engineer(answer)
+                                    engineerInfo = engineer
+                                    console.table(engineer)
+                                })
 
-    
+                                .then(promptMenu)
+                                .then(answer => {
+                                    if (answer.menu === "Finish building team") {
+                                        generatePage(managerInfo, internInfo, engineerInfo)
+                                        console.log(generatePage(managerInfo, internInfo, engineerInfo))
+                                        fs.writeFile('index.html', generatePage(managerInfo, internInfo, engineerInfo), err => {
+                                            if (err) throw err;
+                                        })
+                                    }
+                                })
+
+                        }
+                    })
+            } else {
+                if (answer.menu === "Finish building team") {
+                    generatePage(managerInfo, internInfo, engineerInfo)
+                    console.log(generatePage(managerInfo, internInfo, engineerInfo))
+                    fs.writeFile('index.html', generatePage(managerInfo, internInfo, engineerInfo), err => {
+                        if (err) throw err;
+                    })
+                }
+            }
+
+        }
+
+    })
+
+// fs.writeFile('index.html', generatePage(managerInfo, internInfo, engineerInfo), err => {
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// })
--- src/template.js
+++ src/template.js
@@ -1,4 +1,4 @@
-module.exports = generatePage = (employee, intern, engineer) => {
+module.exports = generatePage = (managerInfo, internInfo, engineerInfo) => {
 return `
 <!DOCTYPE html>
 <html lang="en">
@@ -7,7 +7,7 @@ return `
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="../dist/style.css">
+    <link rel="stylesheet" href="./dist/style.css">
 
     <title>My-Team</title>
 </head>
@@ -16,29 +16,29 @@ return `
     <main>
         <div class="card" style="width: 18rem">
             <ul class="list-group list-group-flush">
-                <li class="card-header">${employee.name}</li>
-                <li class="list-group-item">name</li>
-                <li class="list-group-item">employee iD</li>
-                <li class="list-group-item">email</li>
-                <li class="list-group-item">office</li>
+                <li class="card-header">Manager</li>
+                <li class="list-group-item">name:${managerInfo.name}</li>
+                <li class="list-group-item">id:${managerInfo.id}</li>
+                <li class="list-group-item">email:${managerInfo.email}</li>
+                <li class="list-group-item">${managerInfo.office}</li>
             </ul>
         </div>
         <div class="card" style="width: 18rem">
             <ul class="list-group list-group-flush">
-                <li class="card-header"></li>
-                <li class="list-group-item">name</li>
-                <li class="list-group-item">employee iD</li>
-                <li class="list-group-item">email</li>
-                <li class="list-group-item">office</li>
+                <li class="card-header">Engineer</li>
+                <li class="list-group-item">name:${engineerInfo.name}</li>
+                <li class="list-group-item">id:${engineerInfo.id}</li>
+                <li class="list-group-item">email:${engineerInfo.email}</li>
+                <li class="list-group-item">github:${engineerInfo.github}</li>
             </ul>
         </div>
         <div class="card" style="width: 18rem">
             <ul class="list-group list-group-flush">
                 <li class="card-header">Intern</li>
-                <li class="list-group-item">name</li>
-                <li class="list-group-item">employee iD</li>
-                <li class="list-group-item">email</li>
-                <li class="list-group-item">office</li>
+                <li class="list-group-item">name:${internInfo.name}</li>
+                <li class="list-group-item">id:${internInfo.id}</li>
+                <li class="list-group-item">email:${internInfo.email}</li>
+                <li class="list-group-item">school:${internInfo.school}</li>
             </ul>
         </div>
     </main>
